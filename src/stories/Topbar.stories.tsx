7ca06e570fd31e88ef6a17675c7302f85e6c2297
Kevin <kev.yuan01@gmail.com>
2025-02-21 14:27:27 -0800

    reformat

--- src/stories/Topbar.stories.tsx
+++ src/stories/Topbar.stories.tsx
@@ -1,30 +1,30 @@
-import { StoryObj } from "@storybook/react"
-import { Topbar } from "./Topbar"
+import { StoryObj } from "@storybook/react";
+import { Topbar } from "./Topbar";
 
 export default {
-    title: "Topbar",
-    component: Topbar,
-    argTypes: {
-        theRoute: {control: "text"}
-    }
-}
+  title: "Topbar",
+  component: Topbar,
+  argTypes: {
+    theRoute: { control: "text" },
+  },
+};
 
-type Story = StoryObj<typeof Topbar>
+type Story = StoryObj<typeof Topbar>;
 
 export const Homepage: Story = {
-    args: {
-        theRoute: "/"
-    }
-}
+  args: {
+    theRoute: "/",
+  },
+};
 
 export const Learn: Story = {
-    args: {
-        theRoute: "/learn"
-    }
-}
+  args: {
+    theRoute: "/learn",
+  },
+};
 
 export const Create: Story = {
-    args: {
-        theRoute: "/create"
-    }
-}
+  args: {
+    theRoute: "/create",
+  },
+};
--- src/stories/Topbar.tsx
+++ src/stories/Topbar.tsx
@@ -1,43 +1,40 @@
-import { ArrowBackIos, Home } from "@mui/icons-material"
-import { AppBar, Button, IconButton, Toolbar, Typography } from "@mui/material"
-import { useEffect, useState } from "react"
+import { ArrowBackIos, Home } from "@mui/icons-material";
+import { AppBar, Button, IconButton, Toolbar, Typography } from "@mui/material";
+import { useEffect, useState } from "react";
 
-interface TopbarProps{
-    theRoute: string
+interface TopbarProps {
+  theRoute: string;
 }
 
 export const Topbar: React.FC<TopbarProps> = (props: TopbarProps) => {
+  const [title, setTitle] = useState("Get Started");
+  const [back, setBack] = useState(false);
 
-    const [title, setTitle] = useState("Get Started")
-    const [back, setBack] = useState(false)
+  useEffect(() => {
+    function parseRoute() {
+      const path = props.theRoute.split("/");
+      if (path[path.length - 1] == "") {
+        return;
+      }
+      setBack(path.length > 2);
+      setTitle(
+        `${path[path.length - 1].charAt(0).toUpperCase() + path[path.length - 1].substring(1)}`,
+      );
+    }
+    parseRoute();
+  }, [props.theRoute]);
 
-    useEffect(() => {
-        function parseRoute(){
-            const path = props.theRoute.split("/")
-            if (path[path.length - 1] == ""){
-                return
-            }
-            setBack(path.length > 2)
-            setTitle(`${path[path.length - 1].charAt(0).toUpperCase() + path[path.length - 1].substring(1)}`)
-        }
-        parseRoute()
-    }, [props.theRoute])
-
-    return (
+  return (
     <AppBar position="static">
-        <Toolbar sx={{flex: 1, justifyContent: "space-between"}}>
-            <Typography>
-                <IconButton>
-                    {back ? <ArrowBackIos/> : <Home/>}
-                </IconButton>
-            </Typography>
-            <Typography>
-                {title}
-            </Typography>
-            <Button variant="text" sx={{color: "white"}}>
-                Cancel
-            </Button>
-        </Toolbar>
+      <Toolbar sx={{ flex: 1, justifyContent: "space-between" }}>
+        <Typography>
+          <IconButton>{back ? <ArrowBackIos /> : <Home />}</IconButton>
+        </Typography>
+        <Typography>{title}</Typography>
+        <Button variant="text" sx={{ color: "white" }}>
+          Cancel
+        </Button>
+      </Toolbar>
     </AppBar>
-)
-}
+  );
+};
