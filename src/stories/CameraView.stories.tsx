fe0015b6209af39a5abad33f53a9a3be607cf3f0
Dylan Starink <dylan@starink.com>
2025-02-23 08:50:10 -0800

    Add QR Code Scanner and Camera View (#13)

---
+++ src/stories/CameraView.stories.tsx
@@ -0,0 +1,22 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import { CameraView } from "./CameraView";
+
+const meta = {
+  title: "CameraView",
+  component: CameraView,
+  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
+  tags: ["autodocs"],
+  parameters: {
+    // More on how to position stories at: https://storybook.js.org/docs/configure/story-layout
+    layout: "centered",
+  },
+  argTypes: {
+    setState: { type: "function", control: false },
+  },
+} satisfies Meta<typeof CameraView>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {};
---
+++ src/stories/QrCodeScanner.tsx
@@ -0,0 +1,15 @@
+import { IDetectedBarcode, Scanner } from "@yudiel/react-qr-scanner";
+
+export function QrCodeScanner() {
+  function onScan(codes: IDetectedBarcode[]) {
+    // TODO: Implement logic to handle scanned QR codes to go to
+    // new page or something
+    console.log(codes);
+  }
+
+  return (
+    <div>
+      <Scanner onScan={onScan} />
+    </div>
+  );
+}
