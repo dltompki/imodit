3ef8d5e1f63732a1d8077adeddd3a2afe762d071
Dylan Tompkins <dltompki@outlook.com>
2025-02-24 14:47:42 -0800

    format

--- src/stories/Home.stories.tsx
+++ src/stories/Home.stories.tsx
@@ -1,6 +1,6 @@
-import type { Meta, StoryObj } from '@storybook/react';
+import type { Meta, StoryObj } from "@storybook/react";
 
-import { Home } from './Home';
+import { Home } from "./Home";
 
 const meta = {
   component: Home,
@@ -11,3 +11,4 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {};
+
