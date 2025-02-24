901ed95442a2bb9efe361b4cc3bb9a0dad378768
Dylan Tompkins <dltompki@outlook.com>
2025-02-24 14:37:51 -0800

    roll back some topbar changes

--- src/stories/Topbar.tsx
+++ src/stories/Topbar.tsx
@@ -2,17 +2,25 @@ import { ArrowBackIos, Home } from "@mui/icons-material";
 import { AppBar, Button, IconButton, Toolbar, Typography } from "@mui/material";
 
 interface TopbarProps {
-  title: string;
+  currentRoute: string;
+  backRoute?: string;
 }
 
 export const Topbar: React.FC<TopbarProps> = (props: TopbarProps) => {
+  const pages = props.currentRoute.split("/");
+  const title =
+    pages[pages.length - 1] == ""
+      ? ""
+      : `${pages[pages.length - 1].charAt(0).toUpperCase() + pages[pages.length - 1].substring(1)}`;
   return (
     <AppBar>
       <Toolbar sx={{ flex: 1, justifyContent: "space-between" }}>
         <Typography>
-          <IconButton>{props.back ? <ArrowBackIos /> : <Home />}</IconButton>
+          <IconButton>
+            {props.backRoute ? <ArrowBackIos /> : <Home />}
+          </IconButton>
         </Typography>
-        <Typography>{props.title}</Typography>
+        <Typography>{title}</Typography>
         <Button variant="text" sx={{ color: "white" }}>
           Cancel
         </Button>
