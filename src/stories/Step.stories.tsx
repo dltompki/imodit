99a1f110af8fa13fe791d352a6cdd0799eb8a7e1
Kevin <kev.yuan01@gmail.com>
2025-02-25 03:07:18 -0800

    created feedback box and added some args for steps

--- src/stories/Step.stories.tsx
+++ src/stories/Step.stories.tsx
@@ -3,10 +3,7 @@ import { Step } from "./Step";
 
 export default {
   title: "Steps",
-  component: Step,
-  argTypes: {
-    theRoute: { control: "text" },
-  },
+  component: Step
 };
 
 type Story = StoryObj<typeof Step>;
@@ -14,7 +11,7 @@ type Story = StoryObj<typeof Step>;
 export const StepWithCaption: Story = {
   args: {
     caption: "This is the caption",
-    image: "",
+    image: "https://placehold.co/600x400",
     instructions:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
   },
@@ -22,8 +19,14 @@ export const StepWithCaption: Story = {
 
 export const StepWithNoCaption: Story = {
   args: {
-    image: "",
+    image: "https://placehold.co/600x400",
     instructions:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
   },
 };
+
+export const StepWithNoImage: Story = {
+  args: {
+    image: ""
+  }
+}
