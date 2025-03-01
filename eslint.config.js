bece7826f5b3167e4ae979ffc084dcd2f49c5fda
Kevin <kev.yuan01@gmail.com>
2025-03-01 12:11:13 -0800

    needed for 3d models

--- eslint.config.js
+++ eslint.config.js
@@ -35,6 +35,7 @@ export default tseslint.config(
       ],
       ...react.configs.recommended.rules,
       ...react.configs["jsx-runtime"].rules,
+      "react/no-unknown-property": "off",
     },
     settings: { react: { version: "18.3" } },
   },
