263ba72e5ab6b83a8dbe855cc70d0246c4f939d7
Dylan Starink <dylan@starink.com>
2025-02-28 17:45:21 -0800

    Wire up create project page (#49)

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,23 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import BottomNavBar from "./stories/BottomNavBar";
 import { Home } from "./stories/Home";
+import { CreateProjectHomeScreen } from "./stories/CreateProjectHomeScreen";
+import { ProjectCardProps } from "./stories/ProjectCard";
 
 function App() {
+  // eslint-disable-next-line prefer-const
+  let projects: ProjectCardProps[] = [];
+
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/learn" element={<p>learn</p>} />
         <Route path="/scan" element={<p>scan</p>} />
-        <Route path="/create" element={<p>create</p>} />
+        <Route
+          path="/create"
+          element={<CreateProjectHomeScreen projects={projects} />}
+        />
       </Routes>
       <BottomNavBar />
     </BrowserRouter>
