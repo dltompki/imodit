2b199d64628bc553ae00712d0750404575cc3436
Kevin Yuan <122578058+thekevinWHYuan@users.noreply.github.com>
2025-02-28 17:17:30 -0800

    Feedback box (#24)

--- src/stories/Step.stories.tsx
+++ src/stories/Step.stories.tsx
@@ -1,14 +1,9 @@
 import { StoryObj } from "@storybook/react";
 import { Step } from "./Step";
-import { withRouter } from "storybook-addon-remix-react-router";
 
 export default {
   title: "Steps",
   component: Step,
-  argTypes: {
-    theRoute: { control: "text" },
-  },
-  decorators: [withRouter],
 };
 
 type Story = StoryObj<typeof Step>;
@@ -16,7 +11,7 @@ type Story = StoryObj<typeof Step>;
 export const StepWithCaption: Story = {
   args: {
     caption: "This is the caption",
-    image: "",
+    image: "https://placehold.co/600x400",
     instructions:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
   },
@@ -24,8 +19,14 @@ export const StepWithCaption: Story = {
 
 export const StepWithNoCaption: Story = {
   args: {
-    image: "",
+    image: "https://placehold.co/600x400",
     instructions:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+  },
+};
+
+export const StepWithNoImage: Story = {
+  args: {
+    image: "",
   },
 };
