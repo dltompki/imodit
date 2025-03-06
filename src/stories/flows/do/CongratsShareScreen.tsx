a5faddb4d85aab438bb561c5070e7adda2d18a10
sab115 <113921196+sab115@users.noreply.github.com>
2025-03-05 19:04:30 -0800

    feat: add Snackbar message for shared icon button (#64)

--- src/stories/flows/do/CongratsShareScreen.tsx
+++ src/stories/flows/do/CongratsShareScreen.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { Button, Typography, Box, IconButton } from "@mui/material";
+import React, { useState } from "react";
+import { Button, Typography, Box, IconButton, Snackbar } from "@mui/material";
 import FacebookIcon from "@mui/icons-material/Facebook";
 import InstagramIcon from "@mui/icons-material/Instagram";
 import XIcon from "@mui/icons-material/X";
@@ -9,9 +9,19 @@ import { Topbar } from "../../Topbar.tsx";
 import Pagination from "../../Pagination.tsx";
 import { useNavigate } from "react-router-dom";
 import Paths from "../paths.ts";
+import BottomNavBar from "../../BottomNavBar.tsx";
 
 const CongratsShareScreen: React.FC = () => {
   const navigation = useNavigate();
+  const [snackbarMessage, setSnackbarMessage] = useState("");
+
+  const handleShareClick = () => {
+    setSnackbarMessage("Instruction URL shared!"); // Show the snackbar message
+  };
+
+  const handleCloseSnackbar = () => {
+    setSnackbarMessage(""); // Close the Snackbar
+  };
 
   const handleLeftClick = () => {
     void navigation(Paths.stepThree);
@@ -105,16 +115,28 @@ const CongratsShareScreen: React.FC = () => {
             gap: 2,
           }}
         >
-          <IconButton color="primary" aria-label="facebook">
+          <IconButton
+            color="primary"
+            aria-label="facebook"
+            onClick={handleShareClick}
+          >
             <FacebookIcon />
           </IconButton>
-          <IconButton color="primary" aria-label="instagram">
+          <IconButton
+            color="primary"
+            aria-label="instagram"
+            onClick={handleShareClick}
+          >
             <InstagramIcon />
           </IconButton>
-          <IconButton color="primary" aria-label="x">
+          <IconButton color="primary" aria-label="x" onClick={handleShareClick}>
             <XIcon />
           </IconButton>
-          <IconButton color="primary" aria-label="share">
+          <IconButton
+            color="primary"
+            aria-label="share"
+            onClick={handleShareClick}
+          >
             <ShareIcon />
           </IconButton>
         </Box>
@@ -142,6 +164,14 @@ const CongratsShareScreen: React.FC = () => {
           />
         </Box>
       </Box>
+      {/* Snackbar for sharing */}
+      <Snackbar
+        open={snackbarMessage !== ""} // Show snackbar if message is not empty
+        onClose={handleCloseSnackbar}
+        autoHideDuration={3000}
+        message={snackbarMessage}
+      />
+      <BottomNavBar />
     </>
   );
 };
